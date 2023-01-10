6321de1636f2ea7501743d906add32a122facfd8
vtoman <vtoman@gmail.com>
2023-01-10 21:48:05 +0100

    Adding empty project with required behavior description.

---
+++ ultrasonic-sensor/configurable_distance_trigger/configurable_distance_trigger.ino
@@ -0,0 +1,15 @@
+/*  Connect ultrasonic sensor, two buttons and one LED to Arduino.
+ *  On Arduino start, the LED should be turned off. Whenever the ultrasonic 
+ *  sensor detects object (like hand wave) it should toggle the state of LED.
+ *  Initially the object detection should be as far as possible. Pressing  
+ *  one button, the detection distance should decrease by 10 cm, up to a minimum, 
+ *  with other button, detection distance should increase by 10 cm, up to a maximum.
+ */
+
+void setup() {
+
+}
+
+void loop() {
+
+}
