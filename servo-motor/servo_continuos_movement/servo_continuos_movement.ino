4227ddb6d1c65f1662870ab94cfb38d10303a940
vtoman <vtoman@gmail.com>
2023-01-10 21:27:56 +0100

    Adding empty project with required behavior description.

---
+++ servo-motor/servo_continuos_movement/servo_continuos_movement.ino
@@ -0,0 +1,13 @@
+/*  Perform continuos movement from inital position of 0 deg, to 90 deg
+ *  in duration of 5 second. Then move to 180 degrees, wait 5 seconds,
+ *  perform continuos movement from 180 deg to 90 degrees, and finally
+ *  move to 0 degrees and wait 5 seconds. Loop this behavior.
+ */
+
+void setup() {
+
+}
+
+void loop() {
+
+}
