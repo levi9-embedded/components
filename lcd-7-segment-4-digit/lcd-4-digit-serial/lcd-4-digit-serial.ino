b4ecfe720046c7fea4ab3ac6c31e3298066a84ad
vtoman <vtoman@gmail.com>
2023-01-12 21:58:43 +0100

    Adding placeholder for 4 digit LED example project.

---
+++ lcd-7-segment-4-digit/lcd-4-digit-serial/lcd-4-digit-serial.ino
@@ -0,0 +1,12 @@
+/* Create program that will display numbers from 0000
+ * to 9999 with 200ms in between. Restart from 0000 when 
+ * 9999 is reached.
+ */
+
+void setup() {
+
+}
+
+void loop() {
+
+}
