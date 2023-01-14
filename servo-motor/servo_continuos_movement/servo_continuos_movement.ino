c0893eb75d4a1273da2b8b64cb8c0af12a0e5c9a
gks01 <121801183+gks01@users.noreply.github.com>
2023-01-14 12:46:57 +0100

    Update servo_continuos_movement.ino

--- servo-motor/servo_continuos_movement/servo_continuos_movement.ino
+++ servo-motor/servo_continuos_movement/servo_continuos_movement.ino
@@ -4,10 +4,52 @@
  *  move to 0 degrees and wait 5 seconds. Loop this behavior.
  */
 
-void setup() {
+#include <Servo.h>
 
+//-----------------------------------------------------------------------------------------------|100
+
+int angle = 0;
+int delay_to_90_degrees = 56; // (5 seconds) 5 000 miliseconds / 90 degrees = 56 miliseconds
+
+Servo my_servo;
+
+//-----------------------------------------------------------------------------------------------|100
+
+void setup () 
+{  
+  my_servo.attach (9);
+  my_servo.write (0);
 }
 
-void loop() {
+//-----------------------------------------------------------------------------------------------|100
 
+void loop () 
+{
+  // Initial delay 3s.
+  delay (3000);
+
+  // Move to pos 90 degrees in a period of 5s.
+  for (angle = 0; angle <= 90; angle++)
+  {
+    delay (delay_to_90_degrees);
+    my_servo.write (angle);
+  }
+
+  // Wait for 5s and then move to pos 180 degrees.
+  delay (5000);
+  my_servo.write (180);
+  
+  // Wait for 5s and then move to pos 90 degrees in a period of 5s.
+  delay (5000);
+  for (angle = 180; angle >= 90; angle--)
+  {
+    delay (delay_to_90_degrees);
+    my_servo.write (angle);
+  }
+
+  // Wait for 5s and then move to (initial) pos 0 degrees.
+  delay (5000);
+  my_servo.write (0);
 }
+
+//-----------------------------------------------------------------------------------------------|100
