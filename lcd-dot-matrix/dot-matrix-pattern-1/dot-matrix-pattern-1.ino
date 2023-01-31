0f6b768e6ff5c54a27e4cb435c294421414b5622
vtoman <vtoman@gmail.com>
2023-01-31 17:55:32 +0100

    One dot running from cell 1 to cell 64

--- lcd-dot-matrix/dot-matrix-pattern-1/dot-matrix-pattern-1.ino
+++ lcd-dot-matrix/dot-matrix-pattern-1/dot-matrix-pattern-1.ino
@@ -10,47 +10,59 @@ const int chipSelectPin = 7;
 class DotMatrix {
   public:
     DotMatrix() {
+    }
 
+    void lightOn(byte row, int column) {
+      if (row > 8 && column > 8) return;
+
+      auto col = 0x80 >> (column - 1);
+
+      write(row, static_cast<uint8_t>(col));
+    }
+
+    void clearDisplay() {
+      for (uint8_t row = 0x01; row <= 0x08; row++) {
+        write(row, 0x00);
+      }      
+    }
+
+    void setup() {
+      SPI.begin();
+      
+      pinMode(chipSelectPin, OUTPUT);
+
+      delay(100);
+
+      write(0x0C, 0x01); // Shutdown register, Normal operation
+      write(0x0F, 0x00); // Display test register, Normal display
+      write(0x0B, 0x07); // Scan limit register, Display digits 0, ..., 7
+      write(0x09, 0x00); // Decode mode register, No decode for digits 7 - 0
     }
 
   private:
+    void write(uint8_t reg, uint8_t data) {
+      digitalWrite(chipSelectPin, LOW);
+
+      SPI.transfer(reg); 
+      SPI.transfer(data); 
+
+      digitalWrite(chipSelectPin, HIGH);
+    }
 };
 
+DotMatrix dotMatrix;
+
 void setup() {
-  SPI.begin();
-
-  pinMode(chipSelectPin, OUTPUT);
-
-  delay(100);
-
-  write(0x0C, 0x01); // Shutdown register, Normal operation
-
-  write(0x0F, 0x00); // Display test register, Test display
-
-  write(0x0B, 0x07); // Scan limit, Display digits 0, ..., 7
-
-  write(0x09, 0x00); // Decode mode register, No decode for digits 7 - 0
-
-  write(0x01, 0xFF); // Digit 0 register, set all
-  write(0x02, 0xFF); // Digit 1 register, set all
-  write(0x03, 0xFF); // Digit 2 register, set all
-  write(0x04, 0xFF); // Digit 3 register, set all
-  write(0x05, 0xFF); // Digit 4 register, set all
-  write(0x06, 0xFF); // Digit 5 register, set all
-  write(0x07, 0xFF); // Digit 6 register, set all
-  write(0x08, 0xFF); // Digit 7 register, set all
-}
-
-void write(uint8_t reg, uint8_t data) {
-  digitalWrite(chipSelectPin, LOW);
-
-  SPI.transfer(reg); 
-  SPI.transfer(data); 
-
-  digitalWrite(chipSelectPin, HIGH);
+  dotMatrix.setup();
+  dotMatrix.clearDisplay();
 }
 
 void loop() {
-
-
+  for (byte row = 1; row <= 8; row++) {
+    for (byte column = 1; column <= 8; column++) {
+      dotMatrix.lightOn(row, column);
+      delay(200);
+    }    
+    dotMatrix.clearDisplay();
+  }  
 }
