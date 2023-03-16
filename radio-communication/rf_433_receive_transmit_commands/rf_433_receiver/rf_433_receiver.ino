54cec3581f2840f620325b220df935e4fa6a9f04
Ivan <i.molnar@levi9.com>
2023-03-16 14:57:45 +0100

    rf_433_receive_transmit_commands

--- radio-communication/rf_433_receive_transmit_commands/rf_433_receiver/rf_433_receiver.ino
+++ radio-communication/rf_433_receive_transmit_commands/rf_433_receiver/rf_433_receiver.ino
@@ -6,10 +6,115 @@
  *
  */
 
-void setup() {
-  
+// https://github.com/sparkfun/RF_Links/tree/main/Firmware/Arduino/examples
+
+// RF LINK RECEIVER LIBRARY
+#include <RH_ASK.h>
+
+RH_ASK driver(2000, 2, 3, 0); //2000bps, rxPin = 2, txPin = 3
+
+enum commands
+{
+    command1,
+    command2,
+    command3,
+    command4
+};
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Initialize RF Link Rx Code");
+
+  driver.init();
+
+  //Set pin for LED as status indicator
+  pinMode (13, OUTPUT);
 }
 
-void loop() {
-  
+void morseCodeDot()
+{
+  digitalWrite(13, HIGH);
+  delay(100);
+  digitalWrite(13, LOW);
+  delay(100);
+}
+
+void morseCodeDash()
+{
+  digitalWrite(13, HIGH);
+  delay(300);
+  digitalWrite(13, LOW);
+  delay(100);
+}
+
+void sendSos()
+{
+  // SOS
+  // ...---...
+
+  // set initial state
+  digitalWrite(13, LOW);
+
+  morseCodeDot();
+  morseCodeDot();
+  morseCodeDot();
+
+  morseCodeDash();
+  morseCodeDash();
+  morseCodeDash();
+
+  morseCodeDot();
+  morseCodeDot();
+  morseCodeDot();
+}
+
+void loop()
+{
+  //Set buffer array based on max message length
+  uint8_t buff[RH_ASK_MAX_MESSAGE_LEN];
+
+  //Set variable to indicate buffer length
+  uint8_t buffLen = sizeof(buff);
+
+  if (driver.recv(buff, &buffLen)) // Non-blocking
+  {
+    // Message with a good checksum received.
+    Serial.print("Received message: ");
+
+    buff[buffLen] = 0;
+
+    Serial.println((char*)buff);
+
+    const int recievedValue = atoi((char*)buff);
+
+    switch(recievedValue)
+    {
+      case commands::command1:
+        Serial.println("recieved command1 Led off");
+        digitalWrite(13, LOW);
+        break;
+      case commands::command2:
+        Serial.println("recieved command2 Led on");
+        digitalWrite(13, HIGH);
+        break;
+      case commands::command3:
+        Serial.println("recieved command3 Blink");
+        digitalWrite(13, LOW);
+        delay(100);
+        digitalWrite(13, HIGH);
+        delay(200);
+        digitalWrite(13, LOW);
+        break;
+      case commands::command4:
+        Serial.println("recieved command4 SOS");
+        sendSos();
+        break;
+      default:
+        Serial.println("recieved unknown command");
+        break;
+    }
+
+    delay(100);
+  }
 }
--- radio-communication/rf_433_receive_transmit_commands/rf_433_transmitter/rf_433_transmitter.ino
+++ radio-communication/rf_433_receive_transmit_commands/rf_433_transmitter/rf_433_transmitter.ino
@@ -2,10 +2,68 @@
  *
  */
 
-void setup() {
-  
+ // RF LINK RECEIVER LIBRARY
+#include <RH_ASK.h>
+
+RH_ASK driver(2000, 2, 3, 0);  //2000bps, rxPin = 2, txPin = 3
+
+enum commands
+{
+    command1,
+    command2,
+    command3,
+    command4
+};
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("Initialize RF Link Tx Code");
+  driver.init();
+
+  //Set pin for LED as status indicator
+  pinMode (13, OUTPUT);
+
+  const char* firstMessage = "Hello world!";
+
+  Serial.print("sending message = ");
+  Serial.println(firstMessage);
+
+  driver.send((uint8_t *)firstMessage, strlen(firstMessage));
+  driver.waitPacketSent();
+  delay(200);
 }
 
-void loop() {
+void sendCommand(commands command)
+{
+  digitalWrite(13, true);  //Flash status LED to show transmitting
+
+  char c[20];
+  itoa(command, c, 10);
+  driver.send((uint8_t *)c, strlen(c));
   
+  Serial.print("sending message = ");
+  Serial.println(c);
+
+  driver.waitPacketSent();
+
+  //Turn off status LED
+  digitalWrite(13, false);
+
+  delay(200);
+}
+
+void loop()
+{
+  sendCommand(commands::command1);
+  delay(2000);
+
+  sendCommand(commands::command2);
+  delay(2000);
+
+  sendCommand(commands::command3);
+  delay(2000);
+
+  sendCommand(commands::command4);
+  delay(2000);
 }
