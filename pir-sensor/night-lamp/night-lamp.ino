24a1b0d3820df8fcdbe2599fbfe255dedfda79a5
Viktor Toman <vtoman@gmail.com>
2023-03-30 16:09:29 +0200

    Update night-lamp.ino

--- pir-sensor/night-lamp/night-lamp.ino
+++ pir-sensor/night-lamp/night-lamp.ino
@@ -1,6 +1,14 @@
-/*
+/* Connect output of PIR sensor to the Arduino analog input.
+ * Detection of movement should cause 3.3 volts on the outputs.
  *
+ *   Check:
+ *     - How reliable detection is
+ *     - How far detection works
+ *     - Under which light conditions detection works
+ *     - How long is output on, after detection event
  *
+ * Create app to turn light on for 10 minutes after movement
+ * detection is observed, use integrated led on pin 13 as light.
 */
 
 void setup() {
