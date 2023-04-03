1e569b3c705ac471e82f1de1f58679b4a5c57064
Aleksandar Dezelin <dezelin@gmail.com>
2023-04-03 21:43:27 +0200

    Implemented PIR sensor night lamp functionality.

--- pir-sensor/night-lamp/night-lamp.ino
+++ pir-sensor/night-lamp/night-lamp.ino
@@ -11,10 +11,43 @@
  * detection is observed, use integrated led on pin 13 as light.
 */
 
-void setup() {
+#define pirPin 2
+#define ledPin 13
+boolean detected = false;
+boolean ledOn = false;
+long long lightStarted = -1;
+long long lightTimeout = 600000;
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(pirPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
+   if(digitalRead(pirPin) == HIGH) {
+      if(!detected) {
+         detected = true;
+         digitalWrite(ledPin, HIGH);
+         ledOn = true;
+         lightStarted = millis();
+         Serial.println("Motion detected.");
+         Serial.println("LED turned on.");
+      }
+   }
 
+   if(digitalRead(pirPin) == LOW) {
+      if(detected) {
+         detected = false;
+         Serial.println("Motion ended.");
+      }
+   }
+
+   if(ledOn && millis() - lightStarted >= lightTimeout) {
+      Serial.println("Timeout expired. LED turned off.");
+      digitalWrite(ledPin, LOW);
+      ledOn = false;
+      lightStarted = -1;
+   }
 }
